b691f4ebda56a2fff92b904cce8143c95f23c202
VwoLFy <v_wolfy@mail.ru>
2022-08-26 12:42:01 +0300

    1

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer( (request, response) => {
-    response.write('Vvv');
-    response.end;
+let requestsCount = 0;
+
+const server = http.createServer((request, response) => {
+
+    switch (request.url) {
+        case '/1':
+            response.write(`---------- ${(request.url)} ----------\n\n\t`);
+            break;
+        case '/it':
+            response.write(`---------- ${request.url} ----------\n\n\t`);
+            break;
+        case '/favicon.ico':
+            fs.readFile(__dirname + '\\favicon.ico', function (error, data) {
+                if (error) {
+                  response.statusCode = 404;
+                  response.end('Resourse not found!?');
+                } else {
+                  response.end(data);
+                }
+              })
+            break;
+        default:
+            response.write('Vvv - Please look!\n\n\t');
+            break;
+    }
+
+    if (request.url != '/favicon.ico') {
+        response.write(String(requestsCount++));
+        response.end();
+    }
 })
 
-server.listen(3003);
+server.listen(3003, console.log('Server started 3003'));
